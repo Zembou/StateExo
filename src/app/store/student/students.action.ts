95d25e2381dc38b8629de6f5bc99f7c5b86a87e2
Zembou <58264374+Zembou@users.noreply.github.com>
2024-05-17 10:35:51 +0200

    Student Action and Reducer for success and failure

--- src/app/store/student/students.action.ts
+++ src/app/store/student/students.action.ts
@@ -1,3 +1,12 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
+import { Student } from "src/app/Models/student";
 
 export const gettingStudents = createAction('[Students] Getting Students');
+export const gotStudentsSuccess = createAction(
+    '[Students] Successfully Getting Students',
+    props<{students: Student[]}>()
+);
+export const gotStudentsFailure = createAction(
+    '[Students] Failed Getting Students',
+    props<{error: string}>()
+);
