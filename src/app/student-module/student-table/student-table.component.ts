031fa901b129aa812fd685d886357d94de6b4a66
Zembou <58264374+Zembou@users.noreply.github.com>
2024-05-17 11:05:06 +0200

    Student Component Getting Action Functionnal

--- src/app/student-module/student-table/student-table.component.ts
+++ src/app/student-module/student-table/student-table.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Store, StoreModule } from '@ngrx/store';
+import { Store, StoreModule, select } from '@ngrx/store';
 import * as StudentActions from '../../store/student/students.action';
+import { isLoadingSelector } from 'src/app/store/student/students.selectors';
+import { AppStateInterface } from 'src/app/types/appState.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-student-table',
@@ -8,10 +11,14 @@ import * as StudentActions from '../../store/student/students.action';
   styleUrls: ['./student-table.component.css']
 })
 export class StudentTableComponent implements OnInit{
-  constructor(private store: Store){}
+  loading$: Observable<boolean>;
+  constructor(private store: Store<AppStateInterface>)
+  {
+    this.loading$ = this.store.pipe(select(isLoadingSelector));
+  }
   
   ngOnInit(): void {
-    console.table(this.store);
+    this.store.dispatch(StudentActions.gettingStudents());
   }
 
 }
